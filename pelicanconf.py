f39c94117bcff47c2e9c3188e5c52a72a11ab2e3
karsnaaa <karsanasuresh@gmail.com>
2025-06-23 18:39:02 +0530

    new file added

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = '<Karsna>'  # use your name
+SITENAME        = 'My Python notes' # use title
+GITHUB_USERNAME = 'karsnaaa' # use your username
 
 
 # Blogroll
@@ -12,7 +12,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
+    ("GitHub", "https://github.com/<karsnaaa>/"),
     ("LinkedIn", "<linkedin handle>"),
 )
 
